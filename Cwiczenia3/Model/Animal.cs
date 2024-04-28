f8d17bbb2fdf849ea349a80704af003fcd90bebf
Marek Gargała <s10838@pjwstk.edu.pl>
2024-04-28 15:56:49 +0200

    commit

--- Cwiczenia3/Model/Animal.cs
+++ Cwiczenia3/Model/Animal.cs
@@ -1,10 +1,23 @@
-﻿namespace Cwiczenia3.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cwiczenia3.Model;
 
 public class Animal
 {
+    
+    [Required]
     public int IdAnimal { get; set; }
+   
+    [Required]
+    [MaxLength(200)]
     public string Name { get; set; }
+    [Required]
+    [MaxLength(200)]
     public string Category { get; set; }
+    
+    [MaxLength(200)]
     public string Description { get; set; }
+    [Required]
+    [MaxLength(200)]
     public string Area { get; set; }
 }
